78ae562f2acfea3558ca90dd48dd040150adc2e6
clarence-boddicker <clarence@ocp.finance>
2021-09-02 13:19:41 +0530

    public commit

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from "hardhat/config";
+
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+const config: HardhatUserConfig = {
+  networks: {
+    bsc: {
+      url: process.env.bscRpc || "https://bsc-dataseed.binance.org/",
+      accounts: [process.env["PRIVATE_KEY"]],
+    },
+  },
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 999999,
+      },
+    },
+  },
+};
+
+export default config;
