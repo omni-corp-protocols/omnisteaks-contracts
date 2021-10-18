fa935372adffde7f3d0639a5045a270bcdba61f7
Geek4tek <emil@ocp.finance>
2021-10-18 09:10:31 +0530

    fix issue

--- scripts/configs/bsc.ts
+++ scripts/configs/bsc.ts
@@ -1,28 +1,7 @@
 export const config = {
-  // approvalDelay: "14400", // in seconds
-  // name: "Puff Ape OTAKU-BNB", //"Puff OmnifarmOmnitrade BUSD-USDO-FET",
-  // symbol: "puffApeOTAKU-BNB", //"puffOmnifarmOmnitradeBUSD-USDO-FET",
-  // wantToken: "0x958b2EA8EF2798bD7270Cd757a8756872a6358A2",//"0x1597b1a1E4F387724FbF90D3F7Ff8c883f8e8dE1", // LP Pair
-  // pool: "0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9",//"0x557E864059eA67aEB992b27abbAef0bD1DcF6A39",
-  // poolId: "139",
-  // unirouter: "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607",//"0x10ed43c718714eb63d5aa57b78b54704e256024e",
-  // outputToNativeRoute:["0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95", "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"], //["0x031b41e504677879370e9DBcF937283A8691Fa7f", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"],
-  // outputToLp0Route: ["0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", "0x484215873a674F9af73367a8F94c2C591e997521"],
-  // // [
-  // //   "0x031b41e504677879370e9DBcF937283A8691Fa7f",
-  // //   "0x5801d0e1c7d977d78e4890880b8e579eb4943276",
-  // //   "0xe9e7cea3dedca5984780bafc599bd69add087d56",
-  // // ],
-  // outputToLp1Route: ["0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95", "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"]//["0x031b41e504677879370e9DBcF937283A8691Fa7f", "0x5801d0e1c7d977d78e4890880b8e579eb4943276"],
-
-  //for LINA
   approvalDelay: "14400", // in seconds
-<<<<<<< HEAD
   admin: "0x87bF9c6459C05c136BF74d010691Cd79d36dbE0F",
-  name: "Puff Ellipsis BTCB-renBTC",
-  symbol: "puffEllipsisBTCB-renBTC",
-  unirouter: "0x10ed43c718714eb63d5aa57b78b54704e256024e",
-=======
+  name: "Puff Ape OTAKU-BNB",
+  symbol: "puffApeOTAKU-BNB",
   unirouter: "0x10ed43c718714eb63d5aa57b78b54704e256024e",//"0x10ed43c718714eb63d5aa57b78b54704e256024e",
->>>>>>> fd504e867031aeb16d3d59e029730840dde72beb
 };
