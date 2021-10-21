db68b8d50fb9bf232db3b01a843727120eff2e5d
Geek4tek <emil@ocp.finance>
2021-10-21 13:00:32 +0530

    add puff busd-usdo-ocp

--- scripts/configs/bsc.ts
+++ scripts/configs/bsc.ts
@@ -1,12 +1,13 @@
 export const config = {
   approvalDelay: "14400", // in seconds
   admin: "0x87bF9c6459C05c136BF74d010691Cd79d36dbE0F",
-  name: "Puff Belt BNB",
-  symbol: "puffBeltBNB",
-
-  wantToken: "0xa8bb71facdd46445644c277f9499dd22f6f0a30c",
-  poolId: 338,
+  name: "Puff OmnifarmOmnitrade BUSD-USDO-OCP",
+  symbol: "puffOmnifarmOmnitradeBUSD-USDO-OCP",
+  wantToken: "0x1597b1a1E4F387724FbF90D3F7Ff8c883f8e8dE1",
+  poolId: 0,
   unirouter: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
-  // pool: "0xa8Bb71facdd46445644C277F9499Dd22f6F0A30C",
-  // outputToNativeRoute: ["0x762539b45A1dCcE3D36d080F74d1AED37844b878", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"]
+  pool: "0x5012Dd5cec29D15B82818b24A68E7537b628CC85",
+  outputToNativeRoute: ["0x3C70260eEe0a2bFc4b375feB810325801f289fBd", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"],
+  outputToLp0Route: ["0x3C70260eEe0a2bFc4b375feB810325801f289fBd", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"],
+  outputToLp1Route: ["0x3c70260eee0a2bfc4b375feb810325801f289fbd", "0xe9e7cea3dedca5984780bafc599bd69add087d56", "0x5801d0e1c7d977d78e4890880b8e579eb4943276"],
 };
